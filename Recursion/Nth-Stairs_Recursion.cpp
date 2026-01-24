1143ff7f647baaf159c8efab379d1172af3a705f
ali-hassnain-se <ranaalihassnain07@gmail.com>
2026-01-23 21:34:25 -0800

    Added Nth Stairs problem solution using recursive logic

---
+++ Recursion/Nth-Stairs_Recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int climbStairs(int n) {
+	if(n<=1) {
+		return 1;
+	}
+	
+	return climbStairs(n-1)+climbStairs(n-2);
+}
+
+int main()
+{
+	cout<<climbStairs(4)<<endl;
+	
+	return 0;
+}
