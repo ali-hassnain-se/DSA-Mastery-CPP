4498a4e2a88df6a8afc4cca095a89d250dbfe439
Rana Ali Hassnain <ranaalihassnain07@gmail.com>
2026-03-08 18:54:06 -0700

    Add some changes to display whether Loop Exists or Not

--- Linked_List/Detect_Loop_In_CLL.cpp
+++ Linked_List/Detect_Loop_In_CLL.cpp
@@ -13,6 +13,19 @@ class Node {
 		}
 };
 
+void createLoop(Node* &head, Node* &tail, int x) {
+	if(head==NULL) {
+		head=new Node(x);
+		tail=head;
+		tail->next=head;
+	}
+	else {
+		tail->next=new Node(x);
+		tail=tail->next;
+		tail->next=head;
+	}
+}
+
 bool check(vector<Node*>& visited, Node* curr) {
 	for(int i=0;i<visited.size();i++) {
 		if(visited[i]==curr)
@@ -28,18 +41,34 @@ bool detectLoop(Node* head) {
 	while(curr != NULL) {
 		// If node is already visited
 		if(check(visited, curr))
+		//if loop exist return 1
 		return 1;
 		// else push_back() and increment in curr
 		visited.push_back(curr);
 		curr=curr->next;
 	}
 	
+	//if not exist return 0
 	return 0;
 }
 
 int main()
 {
-	Node* head=NULL;
+	Node* head=NULL, *tail=NULL;
+	int arr[]={1,2,3,4,5};
+	
+	for(int i=0;i<5;i++) {
+		createLoop(head, tail, arr[i]);
+	}
+	
+	int ans=detectLoop(head);
+	
+	if(ans==1) {
+		cout<<"Loop Exists"<<endl;
+	}
+	else {
+		cout<<"Loop Doesn't Exist"<<endl;
+	}
 	
 	return 0;
 }
