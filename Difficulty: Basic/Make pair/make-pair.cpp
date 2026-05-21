ce95e29ca7ed27e8702ad1761d8c6076c6c08708
Rana Ali Hassnain <ranaalihassnain07@gmail.com>
2026-05-21 22:43:38 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Make pair/make-pair.cpp
@@ -0,0 +1,14 @@
+// User function Template for C++
+
+/*
+X, Y: two numbers whose pair is to be returned
+*/
+
+pair<int, int> makePair(int x, int y) {
+    // your code here
+    pair<int, int> p;
+    p=make_pair(x, y);
+    // Return a pair of X and Y
+    
+    return p;
+}
