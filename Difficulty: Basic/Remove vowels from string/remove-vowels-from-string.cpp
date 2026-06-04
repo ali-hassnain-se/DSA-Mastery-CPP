520df1d5c50592e5f5350712e28c51320ae95bd8
Rana Ali Hassnain <ranaalihassnain07@gmail.com>
2026-06-04 23:53:56 +0500

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Remove vowels from string/remove-vowels-from-string.cpp
@@ -0,0 +1,18 @@
+// User function template for C++
+class Solution {
+  public:
+
+    string removeVowels(string& s) {
+        // Your code goes here
+        
+        for(int i=0;i<s.size();i++) {
+            if(s[i]=='a' || s[i]=='e' || s[i]=='i'||
+            s[i]=='o' || s[i]=='u') {
+            s.erase(i,1);
+            i--;
+            }
+        }
+        
+        return s;
+    }
+};
