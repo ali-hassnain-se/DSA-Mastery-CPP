1a361f83e9bbae46e780e316c67f396fbcf0c18d
Rana Ali Hassnain <ranaalihassnain07@gmail.com>
2026-06-21 12:30:52 +0500

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Uncommon characters/uncommon-characters.cpp
+++ Difficulty: Basic/Uncommon characters/uncommon-characters.cpp
@@ -2,8 +2,50 @@ class Solution {
   public:
     string uncommonChars(string& s1, string& s2) {
         // code here
+/* Brute Force Approach, Time Complexity=O(n*m)        
         
-// Time Complexity=O(n+m), Space Complexity=O(1)        
+        string uncommon="";
+        
+        int n=s1.size(), m=s2.size();
+        
+        char ch;
+        
+        for(int i=0;i<n;i++) {
+            bool flag1=false;
+            for(int j=0;j<m;j++) {
+                if(s1[i]==s2[j]) {
+                flag1=true;
+                break;
+                }
+            }
+            
+            if(flag1==false && s1[i]!=ch)
+            uncommon.push_back(s1[i]);
+            
+            ch=s1[i];
+        }
+        
+        for(int i=0;i<m;i++) {
+            bool flag2=false;
+            for(int j=0;j<n;j++) {
+                if(s2[i]==s1[j]) {
+                flag2=true;
+                break;
+                }
+            }
+            
+            if(flag2==false && s2[i]!=ch)
+            uncommon.push_back(s2[i]);
+            
+            ch=s2[i];
+        }
+        
+        sort(uncommon.begin(), uncommon.end());
+        
+        return uncommon;
+*/
+        
+// Optimized Approach, Time Complexity=O(n+m), Space Complexity=O(1)        
         
         vector<bool> unique1(26, 0);
         vector<bool> unique2(26, 0);
