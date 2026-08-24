460f35873a02b649f73652258982ce8d0cf011b1
ali-hassnain-se <ranaalihassnain07@gmail.com>
2026-08-24 22:56:30 +0500

    Added Constructor

---
+++ OOP/constructor_and_destructor.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    // Constructor is a special function that is invoked automatically
+    // at the time of object creation
+    return 0;
+}
